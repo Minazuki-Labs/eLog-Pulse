d8f1f039b29445c37c9cb789621d9ada70d42412
JuneMinazuki <xtfrancis@gmail.com>
2026-03-21 12:48:01 +0800

    Remove boilerplate comand

--- config/ci.rb
+++ config/ci.rb
@@ -1,24 +1,19 @@
-# Run using bin/ci
+# Run using 'bin/ci' to check code quality and security before pushing.
 
 CI.run do
   step "Setup", "bin/setup --skip-server"
 
   step "Style: Ruby", "bin/rubocop"
 
+  # Security Checks
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
+  
+  # Testing
   step "Tests: Rails", "bin/rails test"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
-  # Optional: Run system tests
+# System tests: Uncomment when you start testing in a real browser.
   # step "Tests: System", "bin/rails test:system"
-
-  # Optional: set a green GitHub commit status to unblock PR merge.
-  # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
-  # if success?
-  #   step "Signoff: All systems go. Ready for merge and deploy.", "gh signoff"
-  # else
-  #   failure "Signoff: CI failed. Do not merge or deploy.", "Fix the issues and try again."
-  # end
 end
